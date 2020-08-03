33a392c1b15594208e06cda3e1d272ffd391542b
Tales Alves <5470324-tta13@users.noreply.gitlab.com>
2020-08-03 13:01:04 -0300

    Init unity project

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] private TMP_Text planetNameText;
+
+    private void Start()
+    {
+        planetNameText.text = gameObject.name;
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    private Vector3 _destination;
+
+    public void SetDestination(Transform newDestination)
+    {
+        _destination = newDestination.position;
+        SetRotation();
+    }
+
+    public void Go()
+    {
+        transform.DOMove(_destination, CalculateTime());
+    }
+
+    private float CalculateTime() => Vector3.Distance(_destination, transform.position) / speed;
+
+    private void SetRotation()
+    {
+        var pos = transform.position;
+        var direction = _destination - pos;
+        var quat = Quaternion.LookRotation(Vector3.forward, direction);
+        transform.DORotateQuaternion(quat, 1f);
+    }
+}
